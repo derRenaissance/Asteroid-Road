3e1a7cab33d2b48cc91e4efcf41a547ae0620ef6
monte <montecassino@yandex.ru>
2021-02-23 21:53:53 +0500

    Экран сделан

---
+++ Asteroid Road.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame import *
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 640
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
+BACKGROUND_COLOR = "#0a22b2"
+
+
+def main():
+    screen = pygame.display.set_mode(DISPLAY)
+    pygame.display.set_caption("Asteroid Road")
+    bg = Surface((WIN_WIDTH, WIN_HEIGHT))
+    bg.fill(Color(BACKGROUND_COLOR))
+    while True:
+        for i in pygame.event.get():
+            if i.type == QUIT:
+                raise SystemExit
+            screen.blit(bg, (0, 0))
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
